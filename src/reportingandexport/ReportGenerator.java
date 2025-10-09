c9dc95a107c91c33eba8468a284af7ecb044b258
Nicholas <nicholasang.sg@gmail.com>
2025-10-09 09:34:12 +0800

    redid design logic: added Report Builder and improved ReportGenerator

--- src/reportingandexport/ReportGenerator.java
+++ src/reportingandexport/ReportGenerator.java
@@ -1,7 +1,5 @@
 package reportingandexport;
 
-import java.util.*;
-
 public interface ReportGenerator {
-    public abstract void generate(ArrayList<String> userData);
+    public abstract void generate();
 }
