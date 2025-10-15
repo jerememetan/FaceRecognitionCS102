0320fc1310c0e47a8985253a51466b2d185c4273
Yu-Shan <yushan.chen.2024@computing.smu.edu.sg>
2025-10-15 13:19:48 +0800

    compile and run

--- src/gui/LoginPage.java
+++ src/gui/LoginPage.java
@@ -1,6 +1,10 @@
+package gui;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+
+import gui.MainDashboard;
 
 public class LoginPage extends JFrame{
     private JTextField idField;
