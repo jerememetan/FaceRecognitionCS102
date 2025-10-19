44d41b118dbaf597bd7ec526e38d877daba81937
theejer <leejinrae@gmail.com>
2025-10-20 03:06:03 +0800

    help

---
+++ .history/try_20251020023237.py
@@ -0,0 +1,20 @@
+def group_numbers(list, threshold):
+    copy = list.copy()
+    copy.sort()
+    list_num = []
+    final_list = []
+    total = 0
+    i = 0
+    j = len(list) - 1
+    while i < j:
+        total = 0
+        list_num = []
+        while total < threshold:
+            if total[]
+            if i < j:
+                break
+            
+    print(final_list)
+        
+
+group_numbers([1, 3, 4, 3, 5, 2, 5, 6], 6)
---
+++ .history/try_20251020023916.py
@@ -0,0 +1,24 @@
+def group_numbers(list, threshold):
+    copy = list.copy()
+    copy.sort()
+    list_num = []
+    final_list = []
+    total = 0
+    i = 0
+    j = len(list) - 1
+    while i < j:
+        total = 0
+        list_num = []
+        isI = False
+        while total < threshold:
+            if isI and total + copy[i] <= threshold:
+                total += copy[i]
+                list_num.append(copy[i])
+                
+            if i < j:
+                break
+            
+    print(final_list)
+        
+
+group_numbers([1, 3, 4, 3, 5, 2, 5, 6], 6)
